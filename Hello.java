6c75ffb68d6fb14405b338cc8860e52b7a8cce51
dbsdk0070 <dbsdk0070@naver.com>
2021-03-08 15:04:59 +0900

    added ouput-That's DVCS

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 	public static void main(String[] arsg) {
 		System.out.println("Hellow Hit!!!");
 		System.out.println("What is Git!!");
-	}
+		system.out.println("That's DVCS");
+	} 
 }
