5e2d5b39d0ee1d9a0f936f32f10f8f6f78d92999
dbsdk0070 <76901364+dbsdk0070@users.noreply.github.com>
2021-05-11 16:09:44 +0900

    Delete Hello.java

--- Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello {
-	public static void main(String[] arsg) {
-		System.out.println("Hellow Hit!!!");
-		System.out.println("What is Git!!");
-		system.out.println("That's DVCS");
-		System.out.println("Linux kernel used BitKeeper DVCS");
-
-	} 
-}
